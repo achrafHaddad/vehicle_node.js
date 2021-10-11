a48d7f48e56d0b5c50ad9d05d2cbbff2977bf4e5
achrafHaddad <achrafmail7@gmail.com>
2021-10-11 08:31:20 +0100

    create admin profile

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
 require("dotenv").config();
 const http = require("./app");
+const bcrypt = require("bcrypt");
 
 require("./config/mongo-init");
 const { mailNotif } = require("./middleware/cron");
-// require("./middleware/socket");
 
-// backup.start();
+const User = require("./models/userSchema");
+
+const createAdmin = async () => {
+	try {
+		const adminEmail = "admin@test.com";
+		const password = "12345678";
+
+		const adminExist = await User.findOne({ email: adminEmail });
+		if (adminExist) return;
+
+		const admin = new User({
+			firstName: "admin",
+			lastName: "admin",
+			email: adminEmail,
+			role: "admin",
+		});
+
+		const salt = await bcrypt.genSalt(10);
+		const hashPassword = await bcrypt.hash(password, salt);
+		admin.password = hashPassword;
+
+		await admin.save();
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+createAdmin();
+
 mailNotif.start();
 
 const PORT = process.env.PORT || 3000;
